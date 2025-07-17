fbcb681781730bd3161ca2c65c0a13eb48cbe578
dccupp <dccupp@gmail.com>
2025-07-17 16:39:11 -0400

    updated api.js file

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://api.vampireleaguefootball.com' 
-  : process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 const axiosInstance = axios.create({
   baseURL: API_URL
